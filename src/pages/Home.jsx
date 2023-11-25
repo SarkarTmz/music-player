54fd157dae5c0737d8eab264ab4620a50acf8be2
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-11-25 09:00:55 +0545

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import musics from '../musics/Sushant KC - Sarangi (Official Music Video).mp3'
+import m1 from '../musics/Sushant KC - Sarangi (Official Music Video).mp3'
 
 const Home = () => {
   const music =[
@@ -13,20 +13,70 @@ const Home = () => {
   return (
     <>
       <div className="home container">
-        <div>
-         {
+        <div className="music-main">
+
+         <div className="music">
+          <h2>Music 1</h2>
+           <audio controls>
+            <source src={m1} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+          <div className="music">
+          <h2>Music 1</h2>
+           <audio controls>
+            <source src={m1} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+          <div className="music">
+          <h2>Music 1</h2>
+           <audio controls>
+            <source src={m1} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+          <div className="music">
+          <h2>Music 1</h2>
+           <audio controls>
+            <source src={m1} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+          <div className="music">
+          <h2>Music 1</h2>
+           <audio controls>
+            <source src={m1} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+          <div className="music">
+          <h2>Music 1</h2>
+           <audio controls>
+            <source src={m1} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+          <div className="music">
+          <h2>Music 1</h2>
+           <audio controls>
+            <source src={m1} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+
+        </div>
+      
+        </div>
+
+      {/* {
            music.map((sound) =>(
             <audio controls key={sound.id}>
               <source src={sound.src} type="audio/mpeg"/>
             </audio>
            ))
-         }
-           <audio controls>
-            <source src={musics} type="audio/mpeg" />
-            Your browser does not support the audio tag.
-          </audio>
-        </div>
-      </div>
+         } */}
+
     </>
   )
 }
