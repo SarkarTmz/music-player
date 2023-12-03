933a368efa03726f6f68d46e69c361be7a5b0933
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-12-03 19:23:45 +0545

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -80,13 +80,21 @@ const Home = () => {
             Your browser does not support the audio tag.
           </audio>
           </div>
-              <div className="music">
+          <div className="music">
           <p>Sushant KC - Risaune Bhaye</p>
            <audio controls>
             <source src={m8} type="audio/mpeg" />
             Your browser does not support the audio tag.
           </audio>
           </div>
+          <div className="music">
+          <p>Sushant KC - Risaune Bhaye</p>
+           <audio controls>
+            <source src={m8} type="audio/mpeg" />
+            Your browser does not support the audio tag.
+          </audio>
+          </div>
+
         </div>
       
         </div>
