157185051aa8d52f25e144cf9926ed6150909c85
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-12-22 19:55:12 +0545

    Updated

---
+++ src/Player/audios.js
@@ -0,0 +1,47 @@
+import m1 from '../musics/Sushant KC - Sarangi (Official Music Video).mp3';
+import m2 from '../musics/Ashutosh KC - MAYA.mp3';
+import m3 from '../musics/Je Chhau Timi - Swoopna Suman x Samir Shrestha.mp3';
+import m4 from '../musics/Nachahe ko Hoina -The Edge Band.mp3';
+import m5 from '../musics/RANG - Nikhita Thapa X Brijesh Shrestha.mp3';
+import m6 from '../musics/Sathi - Sushant KC.mp3';
+import m7 from '../musics/Sushant KC - Muskurayera.mp3';
+import m8 from '../musics/Sushant KC - Risaune Bhaye.mp3';
+
+const songsdata = [
+  {
+    title: 'Sushant KC - Sarangi (Official Music Video).mp3',
+    url: m1,
+  },
+  {
+    title: 'Ashutosh KC - MAYA.mp3',
+    url: m2,
+  },
+  {
+    title: 'Je Chhau Timi - Swoopna Suman x Samir Shrestha.mp3',
+    url: m3,
+  },
+  {
+    title: 'Drake - Forever',
+    url: m4,
+  },
+  {
+    title: 'Linkin Park - In the End',
+    url: m5,
+  },
+  {
+    title: 'Travis Scott - Stop Tryna Be God',
+    url: m6,
+  },
+  {
+    title: 'Drake - Forever',
+    url: m7,
+  },
+  {
+    title: 'Linkin Park - In the End',
+    url: m8,
+  },
+
+ 
+];
+
+export { songsdata };
