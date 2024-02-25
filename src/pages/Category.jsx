c8c3111478507c3d680ecd3b7a1c7c3114a96adb
Sarkar <ttpone333@gmail.com>
2024-02-25 21:49:24 +0545

    Updated

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Category = () => {
   return (
     <div className='container'>
-      trandinfg
+      Category
     </div>
   )
 }
--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Trending = () => {
   return (
     <div className='container'>
-      trandinfg
+      tranding
     </div>
   )
 }
